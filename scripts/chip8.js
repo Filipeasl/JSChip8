285e9021195b5253a65a264695e9b435e5243be7
Filipe Almeida Simões <74147258+Filipeasl@users.noreply.github.com>
2022-03-30 15:43:05 -0300

    doc comments

--- scripts/chip8.js
+++ scripts/chip8.js
@@ -1,3 +1,11 @@
+/**
+ * - Reunites all subscripts in a main file and runs the emulation.
+ * 	renderer.js will declare canvas drawing functions/methods.
+ * 	keyboard.js will read keyboard events and translate to operations.
+ * 	speaker.js will declare sound operations according to its frequency.
+ * 	cpu.js is problably the most important, translates rom's operations the cpu execution stack.
+ */
+
 import Renderer from './renderer.js';
 import Keyboard from './keyboard.js';
 import Speaker from './speaker.js';
@@ -8,11 +16,12 @@ const keyboard = new Keyboard();
 const speaker = new Speaker();
 const cpu = new CPU(renderer, keyboard, speaker);
 const urlParams = new URLSearchParams(window.location.search);
+
 const rom = urlParams.get("rom");
 let loop;
-
 let fps = 60, fpsInterval, startTime, now, then, elapsed;
 
+// Begins whole emulation process
 function init() {
 	fpsInterval = 1000 / fps;
 	then = Date.now();
@@ -23,6 +32,8 @@ function init() {
 	loop = requestAnimationFrame(step);
 }
 
+// This function defines de op (operation) execution frequency of the cpu
+// renders animation frames on every step
 function step() {
 	now = Date.now();
 	elapsed = now - then;
@@ -34,4 +45,5 @@ function step() {
 	loop = requestAnimationFrame(step);
 }
 
+// Fires up the emulator
 init();
